6c9cdcb5fab628b977089d72e3d24ea134a37a15
Fred Atilla <36903658+frederatic@users.noreply.github.com>
2019-11-20 23:24:31 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,37 +2,59 @@
 $(function() {
 
     // Populate using an object literal
+    WTF.init({
+    "heading": [
+        "Why don't you fucking research",
+        "Your next fucking experiment should look at",
+        "How about you fucking investigate",
+        "Go fucking study"
+    ],
 
-    // WTF.init({
+    "response": [
+        "Already fucking done",
+        "Give me a-fucking-nother one",
+        "Next fucking experiment",
+        "Nobody fucking cares",
+        "No fucking money in this area"
+    ],
 
-    //     heading: [
-    //         "It's a fucking",
-    //         "Check this shit out, a fucking"
-    //     ],
-    //     response: [
-    //         "Already fucking seen one",
-    //         "Give me a-fucking-nother one",
-    //         "Don't really give a fuck"
-    //     ],
-    //     template: [
-    //         "Big @color @animal",
-    //         "Silly @animal with @color fur"
-    //     ],
-    //     animal: [
-    //         "dog",
-    //         "cat",
-    //         "rabbit"
-    //     ],
-    //     color: [
-    //         "red",
-    //         "blue",
-    //         "green",
-    //         "yellow"
-    //     ]
-    // });
+    "template": [
+        "The difference in @depvar between @group",
+        "The impact of @invar on @depvar",
+        "The effects of @invar on @depvar",
+        "The influence of @invar on @depvar",
+        "The correlation between @invar and @depvar",
+        "The association between @invar and @depvar",
+        "The relation between @invar and @depvar",
+        "The relationship between @invar and @depvar"
+    ],
+
+    "invar": [
+        "alcohol consumption",
+        "LSD",
+        "rabbit"
+    ],
+
+    "depvar": [
+        "social development",
+        "intelligence",
+        "emotional intelligence",
+        "spatial perception"
+    ]
+    
+    "group": [
+        "children",
+        "teens",
+        "elderly",
+        "men and women",
+        "students",
+        "clinically depressed people",
+        "adults"
+    ]    
+});
 
     // Populate using a JSON file
-    WTF.init( 'sample.json' );
+    // WTF.init( 'sample.json' );
 
     // Populate using a Google spreadsheet ID (you must publish it first!)
     // @see https://support.google.com/drive/answer/37579?hl=en
